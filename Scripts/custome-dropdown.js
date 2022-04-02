32de625b71c958cdc6262ed9793d7e74b8778eaa
Vivek Godhani <vivek.godhani@openxcell.info>
2022-04-02 13:16:26 +0530

    Style changes

--- Scripts/custome-dropdown.js
+++ Scripts/custome-dropdown.js
@@ -2,6 +2,8 @@ $(".nav").appendTo('body')
 $(".custome-dropdown > button").click(function (e) {
     e.stopPropagation();
     $(".custome-dropdown > .menu").toggleClass("show")
+    $(".custome-dropdown  #arrowup").toggleClass("show")
+    $(".custome-dropdown #arrowdown").toggleClass("show")
 })
 
 $(".custome-dropdown > .menu > li").click(function () {
@@ -9,7 +11,13 @@ $(".custome-dropdown > .menu > li").click(function () {
     $(".custome-dropdown > button > #changealgobtntxt").text($(this).text());
 
     $(".custome-dropdown > .menu").toggleClass("show")
+    $(".custome-dropdown  #arrowup").toggleClass("show")
+    $(".custome-dropdown #arrowdown").toggleClass("show")
 })
 $("body").click(function () {
-    $(".custome-dropdown > .menu").removeClass("show")
+    if ($(".custome-dropdown > .menu").hasClass("show")) {
+        $(".custome-dropdown > .menu").removeClass("show")
+        $(".custome-dropdown  #arrowup").toggleClass("show")
+        $(".custome-dropdown #arrowdown").toggleClass("show")
+    }
 })
